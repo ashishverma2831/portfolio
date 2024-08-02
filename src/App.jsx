fdb856c49fa78537dcef308cc9e601efc068a2d6
ashishverma2831 <ashishverma510207@gmail.com>
2024-08-02 23:35:58 +0530

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlurIn from './components/magicui/blur-in'
+
+const App = () => {
+  return (
+    <div className='bg-red-400'>
+      <BlurIn word='Hello World' />
+    </div>
+  )
+}
+
+export default App
