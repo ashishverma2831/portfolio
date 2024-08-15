a84085fb287ef75cb1366e7a0b18f2fb6915a63a
ashishverma2831 <ashishverma510207@gmail.com>
2024-08-15 23:07:25 +0530

    feat: Add TypingAnimation component to Skills section
    The code changes include adding the TypingAnimation component to the Skills section of the Home page. This component adds a typing animation effect to the "Tech Stack" heading, enhancing the visual appeal of the section.

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,31 @@ import React from 'react'
 import { Dock, DockIcon } from './components/magicui/dock'
 import { Link } from 'react-router-dom'
 import IconCloud from './components/magicui/icon-cloud'
+import TypingAnimation from './components/magicui/typing-animation'
 
 const App = () => {
 
   const slugs = [
-    "typescript",
-    "javascript",
-    "dart",
     "java",
+    "javascript",
     "react",
-    "flutter",
-    "android",
     "html5",
     "css3",
     "nodedotjs",
-    "express",
-    "nextdotjs",
-    "prisma",
-    "amazonaws",
-    "postgresql",
-    "firebase",
-    "nginx",
-    "vercel",
-    "testinglibrary",
-    "jest",
-    "cypress",
-    "docker",
-    "git",
-    "jira",
-    "github",
-    "gitlab",
-    "visualstudiocode",
+    "mongodb",
     "androidstudio",
-    "sonarqube",
+    "tailwindcss",
+    "bootstrap",
+    "redux",
+    "vite",
+    "nextdotjs",
+    "express",
+    "firebase",
+    "vercel",
+    "render",
+    "git",
+    "github",
+    "visualstudiocode",
     "figma",
   ];
 
@@ -45,39 +37,56 @@ const App = () => {
       <section className='md:max-w-screen-md mx-auto h-[600px] bg-yellow-400' id='education'>Education</section>
       <section className='md:max-w-screen-md mx-auto h-[600px] bg-blue-400' id='projects'>Projects</section>
       <section className='md:max-w-screen-md mx-auto h-[600px] bg-orange-400' id='skills'>
-        <IconCloud iconSlugs={slugs} />
+        <TypingAnimation
+          className="text-4xl font-bold text-black dark:text-white"
+          text="Tech Stack"
+        />
+        <section>
+          <div>
+            {
+              slugs.map((slug, index) => {
+                return <span key={index}
+                className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg"
+                >{slug}</span>
+              })
+            }
+          </div>
+          <div>
+            <IconCloud iconSlugs={slugs} />
+          </div>
+        </section>
       </section>
       <section className='md:max-w-screen-md mx-auto h-[600px] bg-purple-400' id='certificates'>Certificates</section>
       <section className='md:max-w-screen-md mx-auto h-[600px] bg-sky-400' id='subscribe'>Subscribe</section>
       <section className='md:max-w-screen-md mx-auto h-[600px] bg-pink-400' id='contacts'>Contacts</section>
 
-        <Dock className='rounded-full shadow-2xl sticky bottom-8 flex justify-around'>
-          <DockIcon className='hover:bg-stone-300/50'>
-            <a className='flex justify-center items-center w-full h-full' href='/' title='Home'><i className="fa-solid fa-house fa-lg"></i></a>
-          </DockIcon>
-          <DockIcon className='hover:bg-stone-300/50'>
-            <a className='flex justify-center items-center w-full h-full' href='#education' title='Education'><i className="fa-solid fa-laptop-file fa-lg"></i></a>
-          </DockIcon>
-          <DockIcon className='hover:bg-stone-300/50' >
-            <a className='flex justify-center items-center w-full h-full' href='#projects' title='Projects'><i className="fa-solid fa-cube fa-lg"></i></a>
-          </DockIcon>
-          <DockIcon className='hover:bg-stone-300/50' >
-            <a className='flex justify-center items-center w-full h-full' href='#skills' title='Skills'><i className="fa-solid fa-tools fa-lg"></i></a>
-          </DockIcon>
-          <DockIcon className='hover:bg-stone-300/50'>
-            <a className='flex justify-center items-center w-full h-full' href='#certificates' title='Certificates'><i className="fa-solid fa-certificate fa-lg"></i></a>
-          </DockIcon>
-          <span className='h-full w-[1.6px] bg-slate-200'></span>
-          <DockIcon className='hover:bg-stone-300/50'>
-            <a target='_blank' className='flex justify-center items-center w-full h-full' href='https://github.com/ashishverma2831' title='GitHub'><i className="fa-brands fa-github fa-lg"></i></a>
-          </DockIcon>
-          <DockIcon className='hover:bg-stone-300/50'>
-            <a target='_blank' className='flex justify-center items-center w-full h-full' href='https://www.linkedin.com/in/ashishverma2816/' title='LinkedIn'><i className="fa-brands fa-linkedin-in fa-lg"></i></a>
-          </DockIcon>
-          <DockIcon className='hover:bg-stone-300/50'>
-            <a target='_blank' className='flex justify-center items-center w-full h-full' href='https://www.instagram.com/ashishverma2342/' title='Instagram'><i className="fa-brands fa-instagram fa-lg"></i></a>
-          </DockIcon>
-        </Dock>
+      <Dock className='rounded-full shadow-2xl sticky bottom-8 flex justify-around'>
+        <DockIcon className='hover:bg-stone-300/50'>
+          <a className='flex justify-center items-center w-full h-full' href='/' title='Home'><i className="fa-solid fa-house fa-lg"></i></a>
+        </DockIcon>
+        <DockIcon className='hover:bg-stone-300/50'>
+          <a className='flex justify-center items-center w-full h-full' href='#education' title='Education'><i className="fa-solid fa-laptop-file fa-lg"></i></a>
+        </DockIcon>
+        <DockIcon className='hover:bg-stone-300/50' >
+          <a className='flex justify-center items-center w-full h-full' href='#projects' title='Projects'><i className="fa-solid fa-cube fa-lg"></i></a>
+        </DockIcon>
+        <DockIcon className='hover:bg-stone-300/50' >
+          <a className='flex justify-center items-center w-full h-full' href='#skills' title='Skills'><i className="fa-solid fa-tools fa-lg"></i></a>
+        </DockIcon>
+        <DockIcon className='hover:bg-stone-300/50'>
+          <a className='flex justify-center items-center w-full h-full' href='#certificates' title='Certificates'><i className="fa-solid fa-certificate fa-lg"></i></a>
+        </DockIcon>
+        <span className='h-full w-[1.6px] bg-slate-200'></span>
+        <DockIcon className='hover:bg-stone-300/50'>
+          <a target='_blank' className='flex justify-center items-center w-full h-full' href='https://github.com/ashishverma2831' title='GitHub'><i className="fa-brands fa-github fa-lg"></i></a>
+        </DockIcon>
+        <DockIcon className='hover:bg-stone-300/50'>
+          <a target='_blank' className='flex justify-center items-center w-full h-full' href='https://www.linkedin.com/in/ashishverma2816/' title='LinkedIn'><i className="fa-brands fa-linkedin-in fa-lg"></i></a>
+        </DockIcon>
+        <DockIcon className='hover:bg-stone-300/50'>
+          <a target='_blank' className='flex justify-center items-center w-full h-full' href='https://www.instagram.com/ashishverma2342/' title='Instagram'><i className="fa-brands fa-instagram fa-lg"></i></a>
+        </DockIcon>
+      </Dock>
 
     </>
   )
